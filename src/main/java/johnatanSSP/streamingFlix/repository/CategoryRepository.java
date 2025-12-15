fce3f8504a067cbb059f465573d890b7cfda2d8e
Johnatan S S Paixão <johnatan.sousa@gmail.com>
2025-12-15 18:27:04 -0300

    feat: create repository

---
+++ src/main/java/johnatanSSP/streamingFlix/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package johnatanSSP.streamingFlix.repository;
+
+import johnatanSSP.streamingFlix.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
+
