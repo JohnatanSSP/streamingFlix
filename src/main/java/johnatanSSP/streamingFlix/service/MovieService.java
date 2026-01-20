cb18f45e9557cd60b8f54e71df731b24fe158588
Johnatan S S Paixão <johnatan.sousa@gmail.com>
2026-01-20 15:21:19 -0300

    fix: categories and streaming in method save

--- src/main/java/johnatanSSP/streamingFlix/service/MovieService.java
+++ src/main/java/johnatanSSP/streamingFlix/service/MovieService.java
@@ -1,10 +1,13 @@
 package johnatanSSP.streamingFlix.service;
 
+import johnatanSSP.streamingFlix.entity.Category;
 import johnatanSSP.streamingFlix.entity.Movie;
+import johnatanSSP.streamingFlix.entity.Streaming;
 import johnatanSSP.streamingFlix.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -12,12 +15,32 @@ import java.util.List;
 public class MovieService {
 
     private final MovieRepository Repository;
+    private final CategoryService categoryService;
+    private final StreamingService streamingService;
 
     public Movie save(Movie movie) {
+        movie.setCategories(this.findCategories(movie.getCategories()));
+        movie.setStreamings(this.findStreaming(movie.getStreamings()));
         return Repository.save(movie);
     }
 
     public List<Movie> findAll() {
         return Repository.findAll();
+    }
+
+    private List<Category> findCategories(List<Category> categories) {
+        List<Category> categoryFound = new ArrayList<>();
+        categories.forEach(category -> categoryService
+                .findById(category.getId()).ifPresent(categoryFound::add));
+
+        return categoryFound;
+    }
+
+    private List<Streaming> findStreaming(List<Streaming> streamings) {
+        List<Streaming> streamingsFound = new ArrayList<>();
+        streamings.forEach(streaming -> streamingService
+                .findById(streaming.getId()).ifPresent(streamingsFound::add));
+
+        return streamingsFound;
     }
 }
