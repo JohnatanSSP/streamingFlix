9d4f7e4b344a4ad1d89dd81e02fe10e48d655b1c
Johnatan S S Paixão <johnatan.sousa@gmail.com>
2026-01-20 14:50:08 -0300

    feat: implements saved and findAll in controller

--- src/main/java/johnatanSSP/streamingFlix/controller/MovieController.java
+++ src/main/java/johnatanSSP/streamingFlix/controller/MovieController.java
@@ -1,14 +1,35 @@
 package johnatanSSP.streamingFlix.controller;
 
+import johnatanSSP.streamingFlix.controller.request.MovieRequest;
+import johnatanSSP.streamingFlix.controller.response.MovieResponse;
+import johnatanSSP.streamingFlix.entity.Movie;
+import johnatanSSP.streamingFlix.mapper.MovieMapper;
 import johnatanSSP.streamingFlix.repository.MovieRepository;
 import johnatanSSP.streamingFlix.service.MovieService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/streamingflix/movie")
 public class MovieController {
     private final MovieService Service;
+
+    @PostMapping
+    public ResponseEntity<MovieResponse> save(@RequestBody MovieRequest request) {
+        Movie savedMovie = Service.save(MovieMapper.toMovie(request));
+        return ResponseEntity.ok(MovieMapper.toMovieResponse(savedMovie));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MovieResponse>> findAll() {
+        List<Movie> movies = Service.findAll();
+        List<MovieResponse> response = movies.stream()
+                .map(MovieMapper::toMovieResponse)
+                .toList();
+        return ResponseEntity.ok(response);
+    }
 }
