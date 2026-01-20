38c6b9b2cce078b7c3978d097d1613ce92035be5
Johnatan S S Paixão <johnatan.sousa@gmail.com>
2026-01-20 14:47:26 -0300

    feat: implements saved and findAll

--- src/main/java/johnatanSSP/streamingFlix/service/MovieService.java
+++ src/main/java/johnatanSSP/streamingFlix/service/MovieService.java
@@ -1,12 +1,23 @@
 package johnatanSSP.streamingFlix.service;
 
+import johnatanSSP.streamingFlix.entity.Movie;
 import johnatanSSP.streamingFlix.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class MovieService {
 
     private final MovieRepository Repository;
+
+    public Movie save(Movie movie) {
+        return Repository.save(movie);
+    }
+
+    public List<Movie> findAll() {
+        return Repository.findAll();
+    }
 }
